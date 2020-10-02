2fe80a1e31a999d71eda27e6c8e1515be651cd94
Alla <sapsansun@mail.ru>
2020-10-02 12:14:00 +0300

    added file deletion, moving, added more messages for file creation errors

--- server.py
+++ server.py
@@ -38,16 +38,44 @@ def create_file(message):
     if not(check_path(message)) and os.path.exists(directory_to_put):
         os.system('touch ' + local_path)
         res = {"status": "OK", "message": "File creation succeed"}
+    elif check_path(message):
+        res = {"status": "Denied", "message": "File creation did not succeed - file with this name already exists."}
     else:
-        res = {"status": "Denied", "message": "File creation did not succeed - no such directory."}
-        
+        res = {"status": "Denied", "message": "File creation did not succeed - no such directory to put a file."}
+
     return json.dumps(res)
 
 def read_file(message):
     pass
+
 def delete_file(message):
-    pass
+    root = message["args"]["user"]
+    path = message["args"]["path"]
+    local_path = root + path
+
+    if check_path(message):
+        os.remove(local_path)
+        res = {"status": "OK", "message": "File deletion succeed"}
+    else:
+        res = {"status": "Denied", "message": "File deletion did not succeed - no such file."}
+    return json.dumps(res)
+
+def move_file(message):
+    root = message["args"]["user"]
+    src = message["args"]["src"]
+    dst = message["args"]["dst"]
+
+    if os.path.exists(root+src) and not (os.path.exists(root+dst)):
+        os.system('mv ' + root + src + ' ' + root + dst)
+        res = {"status": "OK", "message": "File moving succeed"}
+    elif os.path.exists(root+dst):
+        res = {"status": "Denied", "message": "File moving did not succeed - destination path already exists."}
+    else:
+        res = {"status": "Denied", "message": "File moving did not succeed - no such source path."}
+    return json.dumps(res)
 
 if __name__ == "__main__":
-    message = json.dumps({"command": "touch", "args": {"user": "/Users/admin/Desktop/ds_project/", "path": "test.txt"}})
-    print(create_file(json_read(message)))
+    message1 = json.dumps({"command": "touch", "args": {"user": "/Users/admin/Desktop/ds_project/", "path": "test.txt"}})
+    message2 = json.dumps({"command": "touch",
+    "args": {"user": "/Users/admin/Desktop", "src": "/ds_project/test.txt", "dst": "/ds/test.txt"}})
+    print(move_file(json_read(message2)))
