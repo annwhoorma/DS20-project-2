cf11a2fb7a0ada2cc18603f59dfdaa66b7ed850e
Alla <sapsansun@mail.ru>
2020-10-03 18:24:43 +0300

    added directory deletion, making, listing

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import json
 import os
 import subprocess
+import shutil
 
 
 """ JSON format for received message:
@@ -21,11 +22,10 @@ def check_path(message):
     root = message["args"]["user"]
     path = message["args"]["path"]
 
-    if os.path.exists(root + path):
-        return True
-    else:
-        return False
+    return os.path.exists(root + path)
 
+def check_root(root):
+    return os.path.exists(root)
 
 def json_read(message):
     obj = json.loads(message)
@@ -41,9 +41,9 @@ def create_file(message):
         os.system('touch ' + local_path)
         res = {"status": "OK", "message": "File creation succeed"}
     elif check_path(message):
-        res = {"status": "Denied", "message": "File creation did not succeed - file with this name already exists."}
+        res = {"status": "Failed", "message": "File creation did not succeed - file with this name already exists."}
     else:
-        res = {"status": "Denied", "message": "File creation did not succeed - no such directory to put a file."}
+        res = {"status": "Failed", "message": "File creation did not succeed - no such directory to put a file."}
 
     return json.dumps(res)
 
@@ -56,7 +56,7 @@ def delete_file(message):
         os.remove(local_path)
         res = {"status": "OK", "message": "File deletion succeed"}
     else:
-        res = {"status": "Denied", "message": "File deletion did not succeed - no such file."}
+        res = {"status": "Failed", "message": "File deletion did not succeed - no such file."}
     return json.dumps(res)
 
 def move_file(message):
@@ -68,9 +68,9 @@ def move_file(message):
         os.system('mv ' + root + src + ' ' + root + dst)
         res = {"status": "OK", "message": "File moving succeed"}
     elif os.path.exists(root+dst):
-        res = {"status": "Denied", "message": "File moving did not succeed - destination path already exists."}
+        res = {"status": "Failed", "message": "File moving did not succeed - destination path already exists."}
     else:
-        res = {"status": "Denied", "message": "File moving did not succeed - no such source path."}
+        res = {"status": "Failed", "message": "File moving did not succeed - no such source path."}
     return json.dumps(res)
 
 def info_file(message):
@@ -87,7 +87,7 @@ def info_file(message):
                 "args": {"size": "{}".format(size), "file_status": "{}".format(status),
                 "i-node": "i_node_name", "path": "{}".format(local_path)}}
     else:
-        res = {"status": "Denied", "message": "File info retrieval did not succeed - no such file."}
+        res = {"status": "Failed", "message": "File info retrieval did not succeed - no such file."}
     return json.dumps(res)
 
 def copy_file(message):
@@ -113,15 +113,72 @@ def copy_file(message):
         res = {"status": "OK", "message": "File was copied successfully",
                 "args": {"filename": new_name.split("s/")[-1]}}
     else:
-        res = {"status": "Denied", "message": "File copying did not succeed - no such file."}
+        res = {"status": "Failed", "message": "File copying did not succeed - no such file."}
 
     return json.dumps(res)
 
+def create_directory(message):
+    root = message["args"]["user"]
+    path = message["args"]["path"]
 
+    if not(check_root(root)):
+        res = {"status": "Failed",
+        "message": "Directory creating did not succeed - root directory is not initialized"}
+    else:
+        if not(check_path(message)):
+            local_path = root + path
+            try:
+                os.mkdir(local_path)
+                res = {"status": "OK", "message": "New directory was successfelly created"}
+            except FileNotFoundError:
+                res = {"status": "Failed", "message": "Directory creating did not succeed - invalid input format"}
+        else:
+            res = {"status": "Failed",
+            "message": "Directory creating did not succeed - required path already exists."}
+
+    return json.dumps(res)
+
+def delete_directory(message):
+    root = message["args"]["user"]
+    path = message["args"]["path"]
+    local_path = root + path
+
+    if not(check_root(root)):
+        res = {"status": "Failed",
+        "message": "Directory deleting did not succeed - root directory is not initialized"}
+    else:
+        if path == "":
+            res = {"status": "Failed", "message":
+            "Directory creating did not succeed - cannot delete root directory"}
+        elif check_path(message):
+            shutil.rmtree('{}'.format(local_path), ignore_errors=True)
+            res = {"status": "OK", "message": "Required directory was successfelly deleted"}
+        else:
+            res = {"status": "Failed", "message":
+            "Directory creating did not succeed - no such directory"}
+    return json.dumps(res)
+
+def list_directory(message):
+    root = message["args"]["user"]
+    path = message["args"]["path"]
+    local_path = root + path
+
+    if not(check_root(root)):
+        res = {"status": "Failed",
+        "message": "Directory listing did not succeed - root directory is not initialized"}
+    else:
+        if not(check_path(message)):
+            res = {"status": "Failed", "message":
+            "Directory listing did not succeed - no such directory"}
+        else:
+            listing = subprocess.check_output('ls ' + local_path, shell=True).decode("utf-8").strip().split("\n")
+            res = {"status": "OK",
+            "message": "Required directory was successfelly listed", "args": {"data": "{}".format(listing)}}
+    return json.dumps(res)
 if __name__ == "__main__":
-    message1 = json.dumps({"command": "touch", "args": {"user": "/Users/admin/Desktop/ds/", "path": "test"}})
+    message1 = json.dumps({"command": "touch", "args": {"user": "/Users/admin/Desktop/ds/new_folder", "path": "test1"}})
     message2 = json.dumps({"command": "mv",
     "args": {"user": "/Users/admin/Desktop", "src": "/ds_project/test.txt", "dst": "/ds/test.txt"}})
-    message3 = json.dumps({"command": "info", "args": {"user": "/Users/admin/Desktop/ds/", "path": "test.txt"}})
+    message3 = json.dumps({"command": "info", "args": {"user": "/Users/admin/Desktop/ds/", "path": "/folder"}})
 
-    print(copy_file(json_read(message1)))
+    print(list_directory(json_read(message3)))
